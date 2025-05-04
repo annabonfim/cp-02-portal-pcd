f0e5807e07e31be5b081404403db71e5691732fb
annabonfim <annabonfim@icloud.com>
2025-05-04 19:40:44 -0300

    interface Usuario com nome, email, senha e token, e servico e tipo de botão.

---
+++ portal-pcd/src/types/Servico.ts
@@ -0,0 +1,6 @@
+export interface Servico {
+    id: string
+    titulo: string
+    descricao?: string
+  }
+
---
+++ portal-pcd/src/types/TipoBotao.ts
@@ -0,0 +1,4 @@
+export type TipoBotao = {
+    texto: string
+    onClick: () => void
+  }
